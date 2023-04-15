6017c0aced04c75bc434057b829870cb4afdb502
Lorenzo Servolini <56275768+LorenzoServolini@users.noreply.github.com>
2023-04-16 01:02:24 +0200

    Uploaded files

---
+++ src/pages/Movies.jsx
@@ -0,0 +1,13 @@
+import { Header, MovieList } from "../components";
+import "../styles/movies.css";
+
+const Movies = () => {
+  return (
+    <main>
+      <Header />
+      <MovieList />
+    </main>
+  );
+};
+
+export default Movies;
